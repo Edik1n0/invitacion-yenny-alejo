ff477297283e29c84591420f06ac9c513a11f123
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-06-29 07:29:31 -0500

    db

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,17 @@
-module.exports = {
-    database: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: '',
-        database: 'test'
-    }
-};
-
 // module.exports = {
 //     database: {
-//         host: 'eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//         user: 'e0lwlm13xfcrsty5',
-//         password: 'oyct0qyoqhhet8qz',
-//         database: 'snzxe6ux5pzgm3ao'
+//         host: '127.0.0.1',
+//         user: 'root',
+//         password: '',
+//         database: 'test'
 //     }
 // };
+
+module.exports = {
+    database: {
+        host: 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'orcq3w3h7yoii8nb',
+        password: 'q1f9653ibntf2vww',
+        database: 'bgn8k0dmly9n6i3x'
+    }
+};

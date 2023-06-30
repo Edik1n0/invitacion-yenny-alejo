f8714767d93b337ff85e78f3fa2b63942629500b
Edik1n0 <35901018+Edik1n0@users.noreply.github.com>
2023-06-30 10:53:13 -0500

    audio

--- src/keys.js
+++ src/keys.js
@@ -1,17 +1,17 @@
-module.exports = {
-    database: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: '',
-        database: 'test'
-     }
-};
-
 // module.exports = {
 //     database: {
-//         host: 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//         user: 'yreygsfu3np69vzd',
-//         password: 'ixnu4sua9c2s6htm',
-//         database: 'qhhyjx6iue8ubcbd'
-//     }
+//         host: '127.0.0.1',
+//         user: 'root',
+//         password: '',
+//         database: 'test'
+//      }
 // };
+
+module.exports = {
+    database: {
+        host: 'y5svr1t2r5xudqeq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'yreygsfu3np69vzd',
+        password: 'ixnu4sua9c2s6htm',
+        database: 'qhhyjx6iue8ubcbd'
+    }
+};
